b3544ea0a16d6ebe53b63c118dfc26a17ca3fe76
Fran Gómez (profesor) <fgomrom726@g.educaand.es>
2024-11-12 12:21:53 +0100

    Añado ejercicios corregidos en clase desde el PC del profe

--- Ejercicios/src/programacion/ut2/Ejercicio1.java
+++ Ejercicios/src/programacion/ut2/Ejercicio1.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
+        // Leer
         Scanner sc = new Scanner(System.in);
+        // Escribir
         System.out.println("Introduzca un número");
         int numero = sc.nextInt();
         System.out.println("El número es: " + numero);
