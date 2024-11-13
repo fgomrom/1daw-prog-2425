3dc153679be11272f9ff3936182b4c907c71a6ca
Fran Gómez (profesor) <fgomrom726@g.educaand.es>
2024-11-13 12:57:31 +0100

    Ejercicios 31 y 32

--- Ejercicios/src/programacion/ut2/Ejercicio32.java
+++ Ejercicios/src/programacion/ut2/Ejercicio32.java
@@ -2,7 +2,7 @@ package programacion.ut2;
 
 public class Ejercicio32 {
     public static void main(String[] args) {
-        System.out.println(maximo(21,20));
+        System.out.println(maximo(10,20));
     }
     
     public static int maximo (int x, int y) {
