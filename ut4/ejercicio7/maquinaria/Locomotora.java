f79ca15353740edc71d8139655889c711d48433f
fgomrom <113981624+fgomrom@users.noreply.github.com>
2025-01-22 11:05:45 +0100

    Añado ejercicio 7

---
+++ ut4/ejercicio7/maquinaria/Locomotora.java
@@ -0,0 +1,18 @@
+package programacion.ut4.ejercicio7.maquinaria;
+
+import programacion.ut4.ejercicio7.personal.Mecanico;
+
+public class Locomotora {
+	String matricula;
+	int potencia;
+	int añoFabricacion;
+	Mecanico mec;
+	
+	public Locomotora(String matricula, int potencia, int añoFabricacion, Mecanico mec) {
+		super();
+		this.matricula = matricula;
+		this.potencia = potencia;
+		this.añoFabricacion = añoFabricacion;
+		this.mec = mec;
+	}
+}
